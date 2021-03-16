de1300b5397e992b4e924e37c8641e5295be464e
Conrad S <conrad.schloer@gmail.com>
2021-03-16 21:48:15 +0100

    push progress

--- generate_datapackages/script.py
+++ generate_datapackages/script.py
@@ -1,12 +1,23 @@
 import logging
 
-logging.getLogger("requests").setLevel(logging.WARNING)
-logging.getLogger("urllib3").setLevel(logging.WARNING)
+for name in ["boto", "urllib3", "s3transfer", "boto3", "botocore", "nose", "requests"]:
+    logging.getLogger(name).setLevel(logging.CRITICAL)
+logger = logging.getLogger(__name__)
+import boto3
+
+boto3.set_stream_logger("", logging.CRITICAL)
+
+
+import json
 import pandas as pd
+import csv
 import requests
 import yaml
+import time
+import os
 from dataflows import Flow
-from datapackage_pipelines.lib import update_resource
+from dataflows.base.exceptions import ProcessorError
+from datapackage_pipelines.lib import update_resource, update_package
 from bcodmo_frictionless.bcodmo_pipeline_processors import (
     load,
     update_fields,
@@ -14,7 +25,23 @@ from bcodmo_frictionless.bcodmo_pipeline_processors import (
     dump_to_s3,
 )
 
-dataset_ids = ["3300", "2292"]
+# dataset_ids = ["3300", "2292", "2291"]
+dataset_ids = ["2295"]
+
+BUCKET_NAME = "conrad-migration-test"
+FILENAME = "datasets.csv"
+# Whether the dump to s3 step should be used
+ADD_DUMP = False
+# Whether the list of dataset_ids should be used instead of all datasets
+FILTER = False
+
+
+# Parse the csv file containing a list of datasets
+with open(FILENAME, "r") as csv_file:
+    reader = csv.reader(csv_file)
+    # Ignore the header
+    next(reader)
+    datasets = [dataset for dataset in reader]
 
 
 def generate_data_url(dataset_id):
@@ -22,16 +49,27 @@ def generate_data_url(dataset_id):
     return url
 
 
-def download_data(dataset_id):
-    url = generate_data_url(dataset_id)
+def download_data(url):
     return pd.read_csv(url, sep="\t", comment="#")
 
 
-def get_latlon_fields(dataset_id):
-    url = f"https://lod.bco-dmo.org/sparql?query=PREFIX+rdf%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%3E%0D%0APREFIX+rdfs%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2000%2F01%2Frdf-schema%23%3E%0D%0APREFIX+owl%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2002%2F07%2Fowl%23%3E%0D%0APREFIX+arpfo%3A+%3Chttp%3A%2F%2Fvocab.ox.ac.uk%2Fprojectfunding%23%3E%0D%0APREFIX+dc%3A+%3Chttp%3A%2F%2Fpurl.org%2Fdc%2Felements%2F1.1%2F%3E%0D%0APREFIX+dcat%3A+%3Chttp%3A%2F%2Fwww.w3.org%2Fns%2Fdcat%23%3E%0D%0APREFIX+dcterms%3A+%3Chttp%3A%2F%2Fpurl.org%2Fdc%2Fterms%2F%3E%0D%0APREFIX+foaf%3A+%3Chttp%3A%2F%2Fxmlns.com%2Ffoaf%2F0.1%2F%3E%0D%0APREFIX+geo%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2003%2F01%2Fgeo%2Fwgs84_pos%23%3E%0D%0APREFIX+geosparql%3A+%3Chttp%3A%2F%2Fwww.opengis.net%2Font%2Fgeosparql%23%3E%0D%0APREFIX+odo%3A+%3Chttp%3A%2F%2Focean-data.org%2Fschema%2F%3E%0D%0APREFIX+participation%3A+%3Chttp%3A%2F%2Fpurl.org%2Fvocab%2Fparticipation%2Fschema%23%3E%0D%0APREFIX+prov%3A+%3Chttp%3A%2F%2Fwww.w3.org%2Fns%2Fprov%23%3E%0D%0APREFIX+rs%3A+%3Chttp%3A%2F%2Fjena.hpl.hp.com%2F2003%2F03%2Fresult-set%23%3E%0D%0APREFIX+schema%3A+%3Chttp%3A%2F%2Fschema.org%2F%3E%0D%0APREFIX+sd%3A+%3Chttp%3A%2F%2Fwww.w3.org%2Fns%2Fsparql-service-description%23%3E%0D%0APREFIX+sf%3A+%3Chttp%3A%2F%2Fwww.opengis.net%2Font%2Fsf%23%3E%0D%0APREFIX+skos%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2004%2F02%2Fskos%2Fcore%23%3E%0D%0APREFIX+time%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2006%2Ftime%23%3E%0D%0APREFIX+void%3A+%3Chttp%3A%2F%2Frdfs.org%2Fns%2Fvoid%23%3E%0D%0APREFIX+xsd%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2001%2FXMLSchema%23%3E%0D%0ASELECT+%3Fdataset+%3Flon_column+%3Flon+%3Flat_column+%3Flat%0D%0AWHERE+%7B%0D%0A++%3Flat+%3Flinked+%3Chttp%3A%2F%2Fvocab.nerc.ac.uk%2Fcollection%2FP09%2Fcurrent%2FLATX%2F%3E+.%0D%0A++%3Flon+%3Flinked+%3Chttp%3A%2F%2Fvocab.nerc.ac.uk%2Fcollection%2FP09%2Fcurrent%2FLONX%2F%3E+.%0D%0A++%3Fdplon+odo%3AisInstanceOf+%3Flon+.%0D%0A++%3Fdplon+skos%3AprefLabel+%3Flon_column+.%0D%0A++%3Fdataset+odo%3AstoresValuesFor+%3Fdplon+.%0D%0A++%3Fdplat+odo%3AisInstanceOf+%3Flat+.%0D%0A++%3Fdataset+odo%3AstoresValuesFor+%3Fdplat+.%0D%0A++%3Fdplat+skos%3AprefLabel+%3Flat_column+.%0D%0A++%3Fdataset+a+odo%3ADataset+.%0D%0A++%3Fdataset+odo%3Aidentifier+%3Fdataset_id+.%0D%0A++%3Fdataset_id+a+odo%3ABCODMOIdentifier+.%0D%0A++%3Fdataset_id+odo%3AresolvableURL+%3Fdataset_id_url+.%0D%0A++%3Fdataset_id+odo%3AidentifierValue+%3Fdataset_id_value+.%0D%0A++FILTER+%28STR%28%3Fdataset_id_value%29+%3D+%22{dataset_id}%22%29%0D%0A++FILTER+%28STR%28%3Fdataset_id_url%29+%3D+STR%28%3Fdataset%29%29%0D%0A%7D%0D%0AORDER+BY+%3Fdataset+&output=json"
+def make_sparql_query(url, retry=0):
     r = requests.get(url)
+    if r.status_code != 200:
+        if retry >= 3:
+            raise Exception("SPARQL timed out :(")
+        print(f"Got a status code of {r.status_code}, waiting and retrying...")
+        time.sleep(1)
+        return make_sparql_query(url, retry=retry + 1)
+
     js = r.json()
     res = js.get("results", {}).get("bindings", {})
+    return res
+
+
+def get_latlon_fields(dataset_id):
+    url = os.environ.get("LATLON_QUERY").format(dataset_id)
+    res = make_sparql_query(url)
 
     assert len(res) in [0, 1]
 
@@ -45,10 +83,8 @@ def get_latlon_fields(dataset_id):
 
 
 def get_species_fields(dataset_id):
-    url = f"https://lod.bco-dmo.org/sparql?query=PREFIX+rdf%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%3E%0D%0APREFIX+rdfs%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2000%2F01%2Frdf-schema%23%3E%0D%0APREFIX+owl%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2002%2F07%2Fowl%23%3E%0D%0APREFIX+arpfo%3A+%3Chttp%3A%2F%2Fvocab.ox.ac.uk%2Fprojectfunding%23%3E%0D%0APREFIX+dc%3A+%3Chttp%3A%2F%2Fpurl.org%2Fdc%2Felements%2F1.1%2F%3E%0D%0APREFIX+dcat%3A+%3Chttp%3A%2F%2Fwww.w3.org%2Fns%2Fdcat%23%3E%0D%0APREFIX+dcterms%3A+%3Chttp%3A%2F%2Fpurl.org%2Fdc%2Fterms%2F%3E%0D%0APREFIX+foaf%3A+%3Chttp%3A%2F%2Fxmlns.com%2Ffoaf%2F0.1%2F%3E%0D%0APREFIX+geo%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2003%2F01%2Fgeo%2Fwgs84_pos%23%3E%0D%0APREFIX+geosparql%3A+%3Chttp%3A%2F%2Fwww.opengis.net%2Font%2Fgeosparql%23%3E%0D%0APREFIX+odo%3A+%3Chttp%3A%2F%2Focean-data.org%2Fschema%2F%3E%0D%0APREFIX+participation%3A+%3Chttp%3A%2F%2Fpurl.org%2Fvocab%2Fparticipation%2Fschema%23%3E%0D%0APREFIX+prov%3A+%3Chttp%3A%2F%2Fwww.w3.org%2Fns%2Fprov%23%3E%0D%0APREFIX+rs%3A+%3Chttp%3A%2F%2Fjena.hpl.hp.com%2F2003%2F03%2Fresult-set%23%3E%0D%0APREFIX+schema%3A+%3Chttp%3A%2F%2Fschema.org%2F%3E%0D%0APREFIX+sd%3A+%3Chttp%3A%2F%2Fwww.w3.org%2Fns%2Fsparql-service-description%23%3E%0D%0APREFIX+sf%3A+%3Chttp%3A%2F%2Fwww.opengis.net%2Font%2Fsf%23%3E%0D%0APREFIX+skos%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2004%2F02%2Fskos%2Fcore%23%3E%0D%0APREFIX+time%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2006%2Ftime%23%3E%0D%0APREFIX+void%3A+%3Chttp%3A%2F%2Frdfs.org%2Fns%2Fvoid%23%3E%0D%0APREFIX+xsd%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2001%2FXMLSchema%23%3E%0D%0APREFIX+param%3A+%3Chttp%3A%2F%2Flod.bco-dmo.org%2Fid%2Fparameter%2F%3E%0D%0ASELECT+%3Fdataset+%3Fspecies_column+%3Fparameter%0D%0AWHERE+%7B%0D%0A++VALUES+%3Fparameter+%7B+param%3A976+param%3A994+param%3A1837+param%3A2010+param%3A1869+param%3A476054+param%3A995+param%3A2008+param%3A912+param%3A928+param%3A830+%7D%0D%0A++%3Fdpspecies+odo%3AisInstanceOf+%3Fparameter+.%0D%0A++%3Fdataset+odo%3AstoresValuesFor+%3Fdpspecies+.%0D%0A++%3Fdataset+a+odo%3ADataset+.%0D%0A++%3Fdpspecies+skos%3AprefLabel+%3Fspecies_column+.%0D%0A++%3Fdataset+odo%3Aidentifier+%3Fdataset_id+.%0D%0A++%3Fdataset_id+a+odo%3ABCODMOIdentifier+.%0D%0A++%3Fdataset_id+odo%3AresolvableURL+%3Fdataset_id_url+.%0D%0A++%3Fdataset_id+odo%3AidentifierValue+%3Fdataset_id_value+.%0D%0A++FILTER+%28STR%28%3Fdataset_id_value%29+%3D+%22{dataset_id}%22%29%0D%0A++FILTER+%28STR%28%3Fdataset_id_url%29+%3D+STR%28%3Fdataset%29%29%0D%0A%7D%0D%0AORDER+BY+%3Fdataset+&output=json"
-    r = requests.get(url)
-    js = r.json()
-    res = js.get("results", {}).get("bindings", {})
+    url = os.environ.get("SPECIES_QUERY").format(dataset_id)
+    res = make_sparql_query(url)
 
     return_list = []
     for result in res:
@@ -93,80 +129,198 @@ processor_to_func = {
     "bcodmo_pipeline_processors.add_schema_metadata": add_schema_metadata,
     "bcodmo_pipeline_processors.dump_to_s3": dump_to_s3,
     "update_resource": update_resource.flow,
+    "update_package": update_package.flow,
 }
 
-for dataset_id in dataset_ids:
+counter = 0
 
+completed = []
+
+false_versioned = []
+repeated = []
+found_pipeline = []
+failed_inference = []
+
+
+def generate_and_run_pipeline(
+    title, dataset_id, dataset_version, species, unique_species, lat, lon, retry=False
+):
+    try:
+        """
+        Create the pipeline, starting with load
+        """
+        steps = [
+            {
+                "run": "bcodmo_pipeline_processors.load",
+                "parameters": {
+                    "from": url,
+                    # TODO add filename/clear name as resource name
+                    "name": "res1",
+                    "format": "csv",
+                    "skip_rows": ["#"],
+                    "delimiter": "\t",
+                    "infer_strategy": "strings" if retry else "full",
+                    "cast_strategy": "strings" if retry else "schema",
+                    "override_schema": {"missingValues": ["", "nd"],},
+                },
+            }
+        ]
+
+        # Add the unique species list to each species column
+        processor_fields = {}
+        for i, col_name in enumerate(species):
+            processor_fields[col_name] = {"bcodmo:": {"unique": unique_species[i],}}
+        if len(processor_fields.keys()):
+            steps.append(
+                {
+                    "run": "bcodmo_pipeline_processors.update_fields",
+                    "parameters": {"resources": ["res1"], "fields": processor_fields},
+                }
+            )
+
+        # Add lat lon to the base of the resource
+        if lat and lon:
+            steps.append(
+                {
+                    "run": "update_resource",
+                    "parameters": {
+                        "resources": ["res1"],
+                        "metadata": {"bcodmo:": {"lat_column": lat, "lon_column": lon}},
+                    },
+                }
+            )
+
+        # Add dataset_id and dataset_version into the datapackage
+        steps.append(
+            {
+                "run": "update_package",
+                "parameters": {"version": dataset_version, "id": dataset_id,},
+            }
+        )
+
+        # TODO add dataset_id to identifier (look up in spec), and dataset_version to version
+        if ADD_DUMP:
+            steps.append(
+                {
+                    "run": "bcodmo_pipeline_processors.dump_to_s3",
+                    "parameters": {
+                        "prefix": f"{dataset_id}/{dataset_version}/data",
+                        "force-format": True,
+                        "format": "csv",
+                        "save_pipeline_spec": True,
+                        "temporal_format_property": "outputFormat",
+                        "bucket_name": BUCKET_NAME,
+                        # TODO- ask adam if empty data manager is necessary?
+                        "data_manager": {"name": "", "orcid": "", "submission_id": "",},
+                    },
+                }
+            )
+
+            # Generate the pipeline spec to get added into the dump to s3 step
+            pipeline_spec_str = _get_pipeline_spec(
+                title, "", dataset_id, dataset_version, "v2.10.0", steps
+            )
+
+            steps[-1]["parameters"]["pipeline_spec"] = pipeline_spec_str
+
+        flow_params = []
+        for step in steps:
+            processor = processor_to_func[step["run"]]
+
+            flow_params.append(processor(step["parameters"]))
+
+        r = Flow(*flow_params,).process()
+        return r, retry
+
+    except ProcessorError as e:
+        if not retry:
+            return generate_and_run_pipeline(
+                title,
+                dataset_id,
+                dataset_version,
+                species,
+                unique_species,
+                lat,
+                lon,
+                retry=True,
+            )
+        raise e
+
+
+for dataset in datasets:
+
+    if FILTER and dataset[0] not in dataset_ids:
+        continue
+
+    if counter > 0 and counter % 50 == 0:
+        print(f"Completed {counter} datasets...")
+    counter += 1
+
+    """
+    Set up the initial variables
+    """
+    dataset_id = dataset[0]
+    if dataset_id in completed:
+        repeated.append(dataset_id)
+        continue
+    print(f"Looking at {dataset_id}")
+
+    dataset_version = dataset[1]
+    try:
+        int(dataset_version)
+    except:
+        dataset_version = "1"
+        false_versioned.append(dataset_id)
+
+    url_type = dataset[2]
+    title = dataset[4]
+
+    url = generate_data_url(dataset_id)
+    if url_type != "Primary":
+        url = dataset[3]
+
+    """
+    Make the sparql queries to get lat_lon and species
+    """
     lat, lon = get_latlon_fields(dataset_id)
-    assert (lat and lon) or not (lat and lon)
     species = get_species_fields(dataset_id)
     unique_species = []
     if len(species):
-        df = download_data(dataset_id)
+        df = download_data(url)
         unique_species = get_unique_species(df, species)
-
-    url = generate_data_url(dataset_id)
-    steps = [
-        {
-            "run": "bcodmo_pipeline_processors.load",
-            "parameters": {
-                "from": url,
-                "name": "res1",
-                "format": "csv",
-                "skip_rows": ["#"],
-                "delimiter": "\t",
-                "infer_strategy": "full",
-                "cast_strategy": "schema",
-                "override_schema": {"missingValues": ["", "nd"],},
-            },
-        }
-    ]
-
-    # Add the unique species list to each species column
-    processor_fields = {}
-    for i, col_name in enumerate(species):
-        processor_fields[col_name] = {"bcodmo:": {"unique": unique_species[i],}}
-    if len(processor_fields.keys()):
-        steps.append(
-            {
-                "run": "bcodmo_pipeline_processors.update_fields",
-                "parameters": {"resources": ["res1"], "fields": processor_fields},
-            }
-        )
-
-    # Add lat lon to the base of the resource
-    if lat and lon:
-        steps.append(
-            {
-                "run": "update_resource",
-                "parameters": {
-                    "resources": ["res1"],
-                    "metadata": {"bcodmo:": {"lat_column": lat, "lon_column": lon}},
-                },
-            }
-        )
-
-    # Generate the pipeline spec to get added into the dump to s3 step
-    dataset_version = ""
-    title = "title"
-    pipeline_spec_str = _get_pipeline_spec(
-        title, "", dataset_id, dataset_version, "v2.10.0", steps
+    r, inference_failed = generate_and_run_pipeline(
+        title, dataset_id, dataset_version, species, unique_species, lat, lon,
     )
 
-    print(pipeline_spec_str)
+    if inference_failed:
+        print("Inference failed")
+        failed_inference.append(dataset_id)
 
-    flow_params = []
-    for step in steps:
-        processor = processor_to_func[step["run"]]
-        flow_params.append(processor(step["parameters"]))
-
-    r = Flow(*flow_params,).process()
     print(r[0].descriptor)
     print()
     print()
 
+    completed.append(dataset_id)
+
     # TODO
     """"
     - check if a dataset with a pipeline-spec & datapackage exists in laminar-dump/whoi server
+    - do a find for all pipeline-spec, filter that later to see if there is a pipeline-spec.yaml
+    - in embargo, if dataset_id has been seen before, ignore it
     - create a dump_to_s3 step
     """
+
+print(
+    f"""
+Done!
+
+{len(found_pipeline)} found pipeline-specs
+{len(failed_inference)} failed inference
+{len(false_versioned)} false versions
+{len(repeated)} repeated
+"""
+)
+
+
+with open("failed_inference.json", "w") as fp:
+    json.dumps(failed_inference, fp)
